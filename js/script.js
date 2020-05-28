808bd07406f4b7af48ec5c34c6c0c22dfa9816a1
ahmed sayed <e.eng.ahmedsayed@gmail.com>
2020-05-28 03:07:29 +0200

    add project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+(function () {
+    document.getElementById('menu').addEventListener('click', function(){
+    document.getElementsByClassName("dropmenu")[0].classList.toggle('hidden');
+    });
+})();
